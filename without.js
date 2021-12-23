5dd1efbcd9a4fc4aa9d313a5438970d571097dda
eze1er <papaezechiel@gmail.com>
2021-12-23 21:32:04 +0000

    Insert the inspect library

--- without.js
+++ without.js
@@ -1,16 +1,17 @@
 const assertArraysEqual = function(array1, array2) {
+  const inspect = require('util').inspect;
   // console.log(array1, array2);
   if (array1.length !== array2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
       return false;
     }
   }
-  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  console.log(`✅✅✅ Assertion Passed: ${inspect(array1)} === ${inspect(array2)}`);
   return true;
 };
 
@@ -49,8 +50,8 @@ const without = function(array1, array2) {
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false 
 assertArraysEqual(without([1, 2, 3], [1, 2, 3]), []); // => should PASS
-assertArraysEqual(without([1, 2, 3], [1, 2, 3]), [3]); // => true
-// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(without([1, 2, 3], [1, 2, 3]), []); // => true
+assertArraysEqual(without([1, 2, 3], [3, 2, 1]), []); // => false
 
 assertArraysEqual(without(["1", "2", "3"], ["1", "2", "3"]), []); // => true
 assertArraysEqual(without(["1", "2", "3"], ["1", "2", 3]), [ '3']); // => false
