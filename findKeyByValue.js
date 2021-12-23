2ff928db7a7e9178598b31d0288104413e424045
eze1er <papaezechiel@gmail.com>
2021-12-23 17:20:34 +0000

    First commit

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  return false;
+};
+
+const findKeyByValue = function(array, key) {
+
+  for (element in array) {
+    if (key === array[element]) {
+      console.log(element);
+      return element;
+    }
+  }
+  console.log ('undefined');
+  return undefined;
+}
+// TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
